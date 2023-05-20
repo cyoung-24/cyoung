759dbeeea74094939a809d5d67776f50643bf3fe
cyoung-24 <134125057+cyoung-24@users.noreply.github.com>
2023-05-20 15:27:57 +0700

    slicing

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="bg-gray-100 min-h-screen">
+    <div class="text-[50px] sm:text-[100px] bg-white rounded-xl p-[10px] shadow-xl absolute top-[30%] left-1/3">
+      hello world
+    </div>
+  </div>
+</template>
